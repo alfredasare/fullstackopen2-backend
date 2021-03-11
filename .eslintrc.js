f560d20a3e9b66e8ccaa831629cccc2e87d3c46f
alfredasare <alfredasare101@gmail.com>
2021-03-11 19:37:51 +0000

    Restructure files and folders

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["off", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+    "no-undef": 0,
+    "no-unused-vars": 0,
+  },
+};
---
+++ controllers/notes.js
@@ -0,0 +1,70 @@
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
+
+notesRouter.get("/", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
+  });
+});
+
+notesRouter.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+notesRouter.post("/", (req, res, next) => {
+  const { body } = req;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: "missing content",
+    });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      res.json(savedNote.toJSON());
+    })
+    .catch((error) => next(error));
+});
+
+notesRouter.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  Note.findByIdAndRemove(id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+notesRouter.put("/:id", (req, res, next) => {
+  const body = req.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = notesRouter;
