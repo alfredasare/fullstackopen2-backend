fa41028ec913cdfc96bce073052fc0ff66d9ffd7
alfredasare <alfredasare101@gmail.com>
2021-03-11 13:29:31 +0000

    Deploy fix

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 
-const app = express();
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
 const Note = require("./models/note");
 
 app.use(express.static("build"));
@@ -22,9 +19,9 @@ const requestLogger = (req, res, next) => {
 
 app.use(requestLogger);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello World</h1>");
+// });
 
 app.get("/api/notes", (req, res) => {
   Note.find({}).then((notes) => {
