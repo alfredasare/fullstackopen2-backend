2d618443d72fa64cea73ab3ea052db717993723c
alfredasare <alfredasare101@gmail.com>
2021-03-14 20:00:05 +0000

    Add populate to find queries

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,12 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", async (req, res) => {
-    const notes = await Note.find({});
+    const notes = await Note
+        .find({})
+        .populate('user', {username: 1, name: 1});
+
     res.json(notes);
 });
 
@@ -20,13 +24,19 @@ notesRouter.get("/:id", async (req, res, next) => {
 notesRouter.post("/", async (req, res, next) => {
     const {body} = req;
 
+    const user = await User.findById(body.userId);
+
     const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
+        user: user._id
     });
 
     const savedNote = await note.save();
+    user.notes = user.notes.concat(savedNote._id);
+    await user.save();
+
     res.json(savedNote);
 });
 
