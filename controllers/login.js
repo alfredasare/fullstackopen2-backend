c3e1638fdba641e8b6054297819356f84d533cd2
alfredasare <alfredasare101@gmail.com>
2021-03-14 23:09:05 +0000

    Add expiration time for token

--- controllers/login.js
+++ controllers/login.js
@@ -22,7 +22,12 @@ loginRouter.post('/', async (req, res) => {
         id: user._id
     };
 
-    const token = jwt.sign(userForToken, process.env.SECRET);
+    // token expires in 60*60 seconds, that is, in one hour
+    const token = jwt.sign(
+        userForToken,
+        process.env.SECRET,
+        {expiresIn: 60*60}
+    );
 
     res
         .status(200)
